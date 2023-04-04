24520b506ed6a42019fb4a7b6af82851533716fd
Kisotama <mantel1342@gmail.com>
2023-04-04 08:39:43 +0700

    Add & Display Students

---
+++ Test/Test/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student what = new Student();
+
+            //what.InputInfo();
+
+            //what.DisplayInfo();
+
+            //Console.ReadKey();
+
+            int num = 0;
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Input number of students: ");
+                    num = Int32.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid Number");
+                }
+            }
+
+            Student[] nstu = new Student[num];
+            for (int i = 0; i < nstu.Length; i++)
+            {
+                nstu[i] = new Student();
+                nstu[i].InputInfo();
+
+            }
+
+            for (int i = 0; i < nstu.Length; i++)
+            {
+                nstu[i].DisplayInfo();
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Test/Test/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Student
+    {
+        private String id;
+        private String name;
+        private String age;
+        private String address;
+
+        public Student(string id, string name, string age, string address)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        public Student(String id)
+        {
+            this.id = id;
+        }
+
+        public Student() { }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("\n - Id: " + id);
+            Console.WriteLine("\n - Name: " + name);
+            Console.WriteLine("\n - Age: " + age);
+            Console.WriteLine("\n - Address: " + address);
+        }
+
+        public void InputInfo()
+        {
+            Console.WriteLine("Input Id: ");
+            id = Console.ReadLine();
+            Console.WriteLine("Input Name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Input Age: ");
+            age = Console.ReadLine();
+            Console.WriteLine("Input Address: ");
+            address = Console.ReadLine();
+        }
+
+        public String Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+    }
+
+}
